a6d798a378a9622020b5ac89cee42fff2bb018f3
Mariana <mariana.souza@everymind.com.br>
2021-12-01 15:05:46 -0300

    fix(database/create): variables names

--- database/create.sql
+++ database/create.sql
@@ -2,16 +2,16 @@ create schema compiladoras;
 
 create table compiladoras.projects (
     id serial primary key NOT NULL,
-    brandicon char(50) NOT NULL,
+    brandIcon char(50) NOT NULL,
     title char(50) NOT NULL,
-    descriptio char(200) NOT NULL,
+    description char(200) NOT NULL,
     restrict boolean NOT NULL,
     link char(50) NOT NULL
 )
 
 create table compiladoras.socialNetworks (
     id serial primary key NOT NULL,
-    texto char(50) NOT NULL,
+    text char(50) NOT NULL,
     icon char(50) NOT NULL,
     link char(50) NOT NULL
 );
